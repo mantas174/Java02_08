8afee005014404741628da57cace8cbc5b9cea8d
Mantas <mantas.muzzz@gmail.com>
2018-01-30 15:45:10 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    /* Realizuoti programą, kuri apskaičiuotų kiek automobilis vidutiniškai sunaudoja kuro 100
+        km. Programa turi paprašyti vartotojo įvesti nuvažiuotus km ir kuro sąnaudas.
+        vidurkis = (kuro sąnaudos *100)/ nuvažiuoto atstumo
+    */
+
+
+    }
+}
